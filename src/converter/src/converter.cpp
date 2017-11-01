5ef6695749a3710d7a9d95cd1c19838bb800edc3
Andrey Balandin <vinc@fromru.com>
2017-11-01 11:19:06 +0000

    add converter and publisher to /output

--- src/converter/src/converter.cpp
+++ src/converter/src/converter.cpp
@@ -1,10 +1,18 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h"
+#include "visualization_msgs/Marker.h"
 #include <iostream>
+
+ros::Publisher pub;
 
 void callback(const geometry_msgs::Point::ConstPtr& msg)
 {
   std::cout << "/topic: " << msg->x << " " << msg->y << " " << msg->z << " " << std::endl;
+
+  visualization_msgs::Marker marker;
+  marker.pose.position = *msg;
+  pub.publish(marker);
+
   return;
 }
 
@@ -12,6 +20,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "converter");
   ros::NodeHandle n;
+  pub = n.advertise<visualization_msgs::Marker>("/output", 1000);
   ros::Subscriber sub = n.subscribe("/input", 1000, callback);
   ros::spin();
   return 0;
