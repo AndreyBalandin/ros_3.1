a4b197487511cecf0c810f6a42e5d62a2a1e9d49
Andrey Balandin <vinc@fromru.com>
2017-11-01 14:00:00 +0000

    add marker_id_counter

--- src/converter/src/converter.cpp
+++ src/converter/src/converter.cpp
@@ -1,7 +1,6 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h"
 #include "visualization_msgs/Marker.h"
-#include <iostream>
 
 
 class SubscribeAndPublish
@@ -10,27 +9,41 @@ public:
   SubscribeAndPublish()
   {
     // topic to publish
-    pub_ = n_.advertise<visualization_msgs::Marker>("/output", 1);
+    pub = n.advertise<visualization_msgs::Marker>("/output", 1);
     // topic to subscribe
-    sub_ = n_.subscribe("/input", 1, &SubscribeAndPublish::callback, this);
+    sub = n.subscribe("/input", 1, &SubscribeAndPublish::callback, this);
+
+    marker_id_counter = 0;
   }
 
-  void callback(const geometry_msgs::Point::ConstPtr& input)
+  void callback(const geometry_msgs::Point::ConstPtr& point)
   {
-    // test output
-    std::cout << "/topic: " << input->x << " " << input->y << " " << input->z << " " << std::endl;
-
     // convert geometry_msgs::Point --> visualization_msgs::Marker
-    visualization_msgs::Marker output;
-    output.pose.position = *input;
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "frame";
+    marker.header.stamp = ros::Time();
+    marker.ns = "points";
+    marker.id = marker_id_counter;
+    marker.type = visualization_msgs::Marker::POINTS;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.color.a = 1.0;
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+    marker.points.push_back(*point);
 
-    pub_.publish(output);
+    // publish new marker
+    pub.publish(marker);
+    marker_id_counter += 1;
   }
 
 private:
-  ros::NodeHandle n_;
-  ros::Publisher pub_;
-  ros::Subscriber sub_;
+  ros::NodeHandle n;
+  ros::Publisher pub;
+  ros::Subscriber sub;
+  int marker_id_counter;
 
 };//End of class SubscribeAndPublish
 
