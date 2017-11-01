39faafa56f1c0dbf8933c463bb851c0c4d50302f
Andrey Balandin <vinc@fromru.com>
2017-11-01 12:15:39 +0000

    SubscribeAndPublish

--- src/converter/src/converter.cpp
+++ src/converter/src/converter.cpp
@@ -3,26 +3,47 @@
 #include "visualization_msgs/Marker.h"
 #include <iostream>
 
-ros::Publisher pub;
 
-void callback(const geometry_msgs::Point::ConstPtr& msg)
+class SubscribeAndPublish
 {
-  std::cout << "/topic: " << msg->x << " " << msg->y << " " << msg->z << " " << std::endl;
+public:
+  SubscribeAndPublish()
+  {
+    // topic to publish
+    pub_ = n_.advertise<visualization_msgs::Marker>("/output", 1);
+    // topic to subscribe
+    sub_ = n_.subscribe("/input", 1, &SubscribeAndPublish::callback, this);
+  }
 
-  visualization_msgs::Marker marker;
-  marker.pose.position = *msg;
-  pub.publish(marker);
+  void callback(const geometry_msgs::Point::ConstPtr& input)
+  {
+    // test output
+    std::cout << "/topic: " << input->x << " " << input->y << " " << input->z << " " << std::endl;
 
-  return;
-}
+    // convert geometry_msgs::Point --> visualization_msgs::Marker
+    visualization_msgs::Marker output;
+    output.pose.position = *input;
+
+    pub_.publish(output);
+  }
+
+private:
+  ros::NodeHandle n_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+
+};//End of class SubscribeAndPublish
+
 
 int main(int argc, char **argv)
 {
+  //Initiate ROS
   ros::init(argc, argv, "converter");
-  ros::NodeHandle n;
-  pub = n.advertise<visualization_msgs::Marker>("/output", 1000);
-  ros::Subscriber sub = n.subscribe("/input", 1000, callback);
+
+  //Create an object of class SubscribeAndPublish that will take care of everything
+  SubscribeAndPublish SAPObject;
+
   ros::spin();
+
   return 0;
 }
-
